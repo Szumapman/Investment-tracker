48fd1cccc629d2af72111e16e7fe819918a571f9
Paweł Szumański <pawel.szumanski@outlook.com>
2024-09-07 12:26:53 +0200

    create base main

---
+++ main.py
@@ -0,0 +1,15 @@
+import uvicorn
+from fastapi import FastAPI
+
+from src.routes import auth
+from src.config.constants import API_V1
+
+API = API_V1
+
+app = FastAPI()
+
+app.include_router(auth.router, prefix=API)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
