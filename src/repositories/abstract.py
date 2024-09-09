4df6702514607447516e5c2f6d57b4fe3caed0df
Paweł Szumański <pawel.szumanski@outlook.com>
2024-09-09 11:55:03 +0200

    create AbstractUserRepo - part
    add: get_user_by_email, get_user_by_username, get_user_by_id, create_user, confirm_user_email

---
+++ src/repositories/abstract.py
@@ -0,0 +1,26 @@
+import abc
+
+from src.database.models import User
+from src.schemas.users import UserIn
+
+
+class AbstractUserRepo(abc.ABC):
+    @abc.abstractmethod
+    async def get_user_by_email(self, email: str) -> User | None:
+        pass
+
+    @abc.abstractmethod
+    async def get_user_by_username(self, username: str) -> User | None:
+        pass
+
+    @abc.abstractmethod
+    async def get_user_by_id(self, user_id: int) -> User | None:
+        pass
+
+    @abc.abstractmethod
+    async def create_user(self, user: UserIn) -> User:
+        pass
+
+    @abc.abstractmethod
+    async def confirm_user_email(self, email: str) -> User:
+        pass
