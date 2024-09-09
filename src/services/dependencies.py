118538717c81a9836d215db7d3c422402da5c3ff
Paweł Szumański <pawel.szumanski@outlook.com>
2024-09-09 11:52:36 +0200

    add get_password_handler and get_email_handler

---
+++ src/services/dependencies.py
@@ -0,0 +1,11 @@
+from src.services.abstract import AbstractPasswordHandler, AbstractEmailService
+from src.services.password import BcryptPasswordHandler
+from src.services.email import FastApiEmailService
+
+
+def get_password_handler() -> AbstractPasswordHandler:
+    return BcryptPasswordHandler()
+
+
+def get_email_handler() -> AbstractEmailService:
+    return FastApiEmailService()
