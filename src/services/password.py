8c89b4749de5bb10c7c9e802c4c5ec01b9c6df79
Paweł Szumański <pawel.szumanski@outlook.com>
2024-09-09 11:51:51 +0200

    create BcryptPasswordHandler

---
+++ src/services/password.py
@@ -0,0 +1,11 @@
+import bcrypt
+
+from src.services.abstract import AbstractPasswordHandler
+
+
+class BcryptPasswordHandler(AbstractPasswordHandler):
+    def hash_password(self, password: str) -> str:
+        return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+
+    def verify_password(self, password: str, hashed_password: str) -> bool:
+        return bcrypt.checkpw(password.encode("utf-8"), hashed_password.encode("utf-8"))
