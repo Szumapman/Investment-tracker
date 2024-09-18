76c7203e350e18f9c2a598aa1984414c5e86eed2
Paweł Szumański <pawel.szumanski@outlook.com>
2024-09-18 12:03:49 +0200

    add RedisCache

---
+++ src/database/cache.py
@@ -0,0 +1,26 @@
+import pickle
+
+from redis.asyncio import Redis
+
+from src.database.abstract import AbstractCache
+from src.config.config import settings
+
+
+class RedisCache(AbstractCache):
+    def __init__(self):
+        self.redis_connection = Redis(
+            host=settings.redis_host,
+            port=settings.redis_port,
+            password=settings.redis_password,
+            db=0,
+        )
+
+    async def get_from_cache(self, key: str) -> object | None:
+        return await self.redis_connection.get(key)
+
+    async def set_to_cache(self, key: str, value: object, expire: int) -> None:
+        await self.redis_connection.set(key, pickle.dumps(value))
+        await self.redis_connection.expire(key, expire)
+
+    async def delete_from_cache(self, key: str) -> None:
+        await self.redis_connection.delete(key)
