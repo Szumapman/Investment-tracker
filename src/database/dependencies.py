3f5361139887ec56cc46dc5520a04d2779a97a81
Paweł Szumański <pawel.szumanski@outlook.com>
2024-09-18 12:04:18 +0200

    add get_redis and get_cache

--- src/database/dependencies.py
+++ src/database/dependencies.py
@@ -1,6 +1,38 @@
+from redis.asyncio import Redis
+
 from src.database.db import get_db
 from src.repositories.abstract import AbstractUserRepo
 from src.repositories.users import PostgresUserRepo
+from src.database.abstract import AbstractCache
+from src.database.cache import RedisCache
+from src.config.config import settings
+
+
+def get_redis(**kwargs) -> Redis:
+    """
+    Function to get Redis.
+
+    Args:
+        **kwargs: redis connection parameters - except: host, port and password
+    Returns:
+        Redis instance
+    """
+    redis = Redis(
+        host=settings.redis_host,
+        port=settings.redis_port,
+        password=settings.redis_password,
+        **kwargs,
+    )
+    return redis
+
+
+def get_cache() -> AbstractCache:
+    """
+    Function to get cache.
+    Returns:
+        Cache instance
+    """
+    return RedisCache()
 
 
 def get_user_repo() -> AbstractUserRepo:
