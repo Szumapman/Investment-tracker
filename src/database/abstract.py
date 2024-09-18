5994d7a97962a5ec50852954deba413c11520f92
Paweł Szumański <pawel.szumanski@outlook.com>
2024-09-18 12:03:31 +0200

    add AbstractCache

---
+++ src/database/abstract.py
@@ -0,0 +1,15 @@
+import abc
+
+
+class AbstractCache(abc.ABC):
+    @abc.abstractmethod
+    async def get_from_cache(self, key: str) -> object | None:
+        pass
+
+    @abc.abstractmethod
+    async def set_to_cache(self, key: str, value: object, expire: int) -> None:
+        pass
+
+    @abc.abstractmethod
+    async def delete_from_cache(self, key: str) -> None:
+        pass
