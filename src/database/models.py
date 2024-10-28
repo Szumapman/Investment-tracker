af99746b91a479bf326c957b4325ed80df5ad446
Paweł Szumański <pawel.szumanski@outlook.com>
2024-10-28 11:54:19 +0100

    add asset_name + migration

--- src/database/models.py
+++ src/database/models.py
@@ -165,6 +165,7 @@ class Asset(Base):
     account_id = Column(
         Integer, ForeignKey("accounts.id", ondelete="CASCADE"), nullable=False
     )
+    asset_name = Column(String(10), nullable=False)
     purchase_amount = Column(Float, nullable=False)
     purchase_date = Column(DateTime(timezone=True), server_default=func.now())
     purchase_share_price = Column(Float, nullable=False)
