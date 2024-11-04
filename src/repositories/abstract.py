39d8e9c9da39738d9c4b8809a7c9d08a5aaaf56f
Paweł Szumański <pawel.szumanski@outlook.com>
2024-11-04 14:03:51 +0100

    add AbstractCurrencyInvestRepo

--- src/repositories/abstract.py
+++ src/repositories/abstract.py
@@ -1,9 +1,11 @@
 import abc
 from datetime import datetime
 
-from src.database.models import User, RefreshToken, Account
+from src.database.models import User, RefreshToken, Account, CurrencyInvest, Transaction
 from src.schemas.users import UserIn
 from src.schemas.accounts import AccountIn
+from src.schemas.currency_invests import CurrencyInvestToBuy
+from src.schemas.transactions import TransactionIn
 
 
 class AbstractUserRepo(abc.ABC):
@@ -42,7 +44,7 @@ class AbstractTokenRepo(abc.ABC):
     @abc.abstractmethod
     async def get_refresh_token(self, refresh_token: str) -> RefreshToken | None:
         pass
-    
+
     @abc.abstractmethod
     async def get_refresh_tokens(self, user_id: int) -> list[RefreshToken]:
         pass
@@ -65,19 +67,45 @@ class AbstractAccountRepo(abc.ABC):
     @abc.abstractmethod
     async def get_account_by_id(self, account_id: int) -> Account:
         pass
-    
+
     @abc.abstractmethod
     async def get_accounts(self, user_id: int) -> list[Account]:
         pass
-    
+
     @abc.abstractmethod
     async def create_account(self, user_id: int, account_in: AccountIn) -> Account:
         pass
-    
+
     @abc.abstractmethod
     async def update_funds(self, account_id: int, amount: float) -> Account:
         pass
-    
+
     @abc.abstractmethod
     async def delete_account(self, account_id: int) -> None:
         pass
+
+
+class AbstractCurrencyInvestRepo(abc.ABC):
+    @abc.abstractmethod
+    async def get_currency_invest_by_id(
+        self, currency_invest_id: int
+    ) -> CurrencyInvest:
+        pass
+
+    # @abc.abstractmethod
+    # async def get_currency_invests(self, account_id: int) -> list[CurrencyInvest]:
+    #     pass
+
+    @abc.abstractmethod
+    async def create_currency_invest(
+        self, transaction_in: TransactionIn, currency_invest_to_buy: CurrencyInvestToBuy
+    ) -> CurrencyInvest:
+        pass
+
+    # @abc.abstractmethod
+    # async def update_currency_invest(self, currency_invest_id: int, currency_invest_to_buy: CurrencyInvestToBuy) -> CurrencyInvest:
+    #     pass
+
+    # @abc.abstractmethod
+    # async def delete_currency_invest(self, currency_invest_id: int) -> None:
+    #     pass
